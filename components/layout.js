908b41e1bd61f285fd0053f57debabb71b89ed87
papa-react <makiya112545@gmail.com>
2022-02-24 23:28:26 +0300

    style layouts

--- components/layout.js
+++ components/layout.js
@@ -3,10 +3,10 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
     return (
-        <>
+        <div className="mx-auto max-w-screen-2xl  ">
             <Navbar />
             <main>{children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
