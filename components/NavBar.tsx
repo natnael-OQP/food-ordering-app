6c2435e5d6f461467a76f93edf63aab47593ba22
papa-react <makiya112545@gmail.com>
2022-02-25 11:44:01 +0300

    Navbar  component completed

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,31 +2,63 @@ import Image from 'next/image'
 
 const NavBar = () => {
     return (
-        <div className="sticky top-0 flex h-14 items-center justify-between bg-red-600 px-4 sm:px-10">
-            {/* items */}
+        <div className="sticky top-0 z-50 flex h-[70px] items-center justify-between bg-red-600 px-4 shadow-md md:px-5 xl:px-10">
+            {/* left */}
             <div className="flex items-center space-x-4">
-                <div className="h-9 w-9 rounded-full bg-white p-1">
-                    <div className="relative h-8 w-8 overflow-hidden">
+                <div className="flex h-11 w-11 items-center justify-center rounded-full bg-white">
+                    <div className="relative m-[10px] h-8 w-8 overflow-hidden rounded-full ">
                         <Image
                             layout="fill"
-                            objectFit="cover"
+                            objectFit="contain"
                             src="/img/telephone.png"
-                            alt="tele"
+                            alt="telephone"
                         />
                     </div>
                 </div>
-                {/* texts */}
                 <div>
-                    {/* text */}
-                    <div>ORDER NOW!</div>
-                    {/* text */}
-                    <div></div>
+                    <div className="text-sm font-normal text-white ">
+                        ORDER NOW!
+                    </div>
+                    <div className="text-base font-bold text-white ">
+                        0941248799
+                    </div>
                 </div>
             </div>
-            {/* items */}
-            <div>center</div>
-            {/* items */}
-            <div>Right</div>
+            {/* center */}
+            <div className="hidden flex-grow md:inline-block">
+                <ul className="hidden items-center  justify-center space-x-2 md:flex  xl:space-x-5 ">
+                    <li className="listItem">HomePage</li>
+                    <li className="listItem">Product</li>
+                    <li className="listItem">Menu</li>
+                    <div className="relative  h-11 w-28 overflow-hidden rounded-full ">
+                        <Image
+                            layout="fill"
+                            objectFit="cover"
+                            src="/img/logo.png"
+                            alt="logo"
+                        />
+                    </div>
+
+                    <li className="listItem">Events</li>
+                    <li className="listItem">Blog</li>
+                    <li className="listItem">Contact</li>
+                </ul>
+            </div>
+            {/* right */}
+            <div>
+                <div className="relative  h-7 w-7  rounded-full ">
+                    <Image
+                        layout="fill"
+                        className="m-1"
+                        objectFit="contain"
+                        src="/img/cart.png"
+                        alt="cart"
+                    />
+                    <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-white text-xs font-semibold text-red-600">
+                        3
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
