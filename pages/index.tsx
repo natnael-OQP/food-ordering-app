1ede05db4f4877fff066ec14382cada3151b3e2a
papa-react <makiya112545@gmail.com>
2022-02-26 17:05:43 +0300

    clean code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { ReactElement } from 'react'
 import Featured from '../components/Featured'
-import Footer from '../components/Footer'
 import Layout from '../components/layout'
-import NavBar from '../components/NavBar'
 import ProductList from '../components/ProductList'
 
 export default function Home() {
