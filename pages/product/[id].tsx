0df0042c7e9699a5ce4c58a619ab967faa3c8401
papa-react <makiya112545@gmail.com>
2022-02-26 22:34:53 +0300

    products page completed

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React, { ReactElement, useState } from 'react'
+import CheckBox from '../../components/CheckBox'
 import Layout from '../../components/layout'
 
 const Product = () => {
@@ -15,8 +16,8 @@ const Product = () => {
     }
 
     return (
-        <div className=" grid h-[87vh] grid-cols-1 py-5 md:grid-cols-2 md:py-20  xl:h-[calc(100vh_-_70px)]">
-            <div className="relative h-[320px] w-full xl:h-[500px] ">
+        <div className=" grid h-full grid-cols-1 pt-5 md:h-[95vh] md:grid-cols-2 md:py-10  xl:h-[calc(100vh_-_70px)]">
+            <div className="relative h-[250px] w-full md:h-[350px] xl:h-[500px] ">
                 <Image
                     layout="fill"
                     objectFit="contain"
@@ -24,8 +25,8 @@ const Product = () => {
                     alt="logo"
                 />
             </div>
-            <div className=" flex flex-col space-y-5 px-2 md:mt-0 md:space-y-8 md:px-0 ">
-                <h1 className="text-5xl font-bold text-gray-800">
+            <div className=" flex flex-col space-y-3 px-2 md:mt-0 md:space-y-8 md:px-0 ">
+                <h1 className="text-3xl font-bold text-gray-800 md:text-5xl">
                     {pizza.name}
                 </h1>
                 <span className="text-2xl font-semibold text-gray-600">
@@ -83,6 +84,27 @@ const Product = () => {
                             Large
                         </span>
                     </div>
+                </div>
+                {/* ingredient  */}
+                <h3 className=" text-3xl font-bold text-gray-900 ">
+                    Choose the size
+                </h3>
+                <div className="grid grid-cols-2 gap-x-2 gap-y-2 xl:grid-cols-4">
+                    <CheckBox name="2X Ingredient" />
+                    <CheckBox name="Extra Cheese" />
+                    <CheckBox name="Spicy Sauce" />
+                    <CheckBox name="Garlic Sauce" />
+                </div>
+                {/* add to cart button */}
+                <div className="mt-4 flex space-x-4">
+                    <input
+                        type="number"
+                        className="h-9 w-16 rounded-sm border border-gray-500 pl-[2px] text-xl "
+                        defaultValue={1}
+                    />
+                    <button className="transform-gpu cursor-pointer bg-red-500 px-3 text-base font-semibold text-gray-50 shadow-lg outline-none transition duration-200 ease-in-out  hover:scale-105 active:scale-95">
+                        Add To Cart
+                    </button>
                 </div>
             </div>
         </div>
