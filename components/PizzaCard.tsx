9c8c8c9d57f354f012e0689f4d73f40664429198
papa-react <makiya112545@gmail.com>
2022-02-28 16:09:31 +0300

    distract products

--- components/PizzaCard.tsx
+++ components/PizzaCard.tsx
@@ -1,24 +1,35 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+import { IProduct } from '../type'
 
-const PizzaCard = () => {
+interface Props {
+    product: IProduct
+}
+
+const PizzaCard: FC<Props> = ({ product }) => {
     return (
-        <div className="flex min-w-[200px] cursor-pointer flex-col items-center space-y-2 rounded-lg bg-white py-5 px-3 shadow-md">
-            <div className="relative h-[200px] w-full">
-                <Image
-                    layout="fill"
-                    objectFit="contain"
-                    src="/img/pizza.png"
-                    alt="logo"
-                />
+        <Link href={`/product/${product._id}`} passHref>
+            <div className="flex min-w-[200px] cursor-pointer flex-col items-center space-y-2 rounded-lg bg-white py-5 px-3 shadow-md">
+                <div className="relative h-[200px] w-full">
+                    <Image
+                        layout="fill"
+                        objectFit="contain"
+                        src={product.image}
+                        alt={product.title}
+                    />
+                </div>
+                <h1 className="text-xl font-bold text-red-500 md:text-lg ">
+                    {product.title}
+                </h1>
+                <span className="text-lg font-semibold text-gray-700">
+                    {product?.prices[0]}
+                </span>
+                <p className="mx-auto max-w-[65vw] text-center text-sm font-normal text-gray-500">
+                    {product.description}
+                </p>
             </div>
-            <h1 className="text-xl font-bold text-red-500 md:text-lg ">
-                Super Pizza
-            </h1>
-            <span className="text-lg font-semibold text-gray-700">$350.5</span>
-            <p className="mx-auto max-w-[65vw] text-center text-sm font-normal text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing.
-            </p>
-        </div>
+        </Link>
     )
 }
 
