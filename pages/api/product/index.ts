3d157a282a067f6f8f1fbd2024b248a75a12eb56
papa-react <makiya112545@gmail.com>
2022-02-28 11:15:55 +0300

    create(post) and get  product data in to monogodb database

---
+++ pages/api/product/index.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+// db connect
+import dbConnect from '../../../lib/dbConnect'
+// model
+import Product from '../../../model/Product'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const { method } = req
+    dbConnect()
+
+    if (method === 'GET') {
+        try {
+            const product = await Product.find()
+            res.status(200).json(product)
+        } catch (error) {
+            res.status(5000).json(error)
+        }
+    }
+
+    if (method === 'POST') {
+        try {
+            const product = await Product.create(req.body)
+            res.status(200).json(product)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
