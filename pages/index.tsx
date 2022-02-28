e0e7d4c6f51ae9fc60877d1bdfc5f055ed8e176a
papa-react <makiya112545@gmail.com>
2022-02-28 16:09:50 +0300

    get data from apis

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { ReactElement } from 'react'
 import Featured from '../components/Featured'
 import Layout from '../components/layout'
 import ProductList from '../components/ProductList'
-
-export default function Home() {
+import axios from 'axios'
+import { IProduct } from '../type'
+interface Props {
+    products: IProduct[],
+}
+export default function Home({ products }: Props) {
     return (
         <div>
             <Head>
@@ -12,11 +17,20 @@ export default function Home() {
                 <meta name="description" content="best Pizza Shop in Addise" />
             </Head>
             <Featured />
-            <ProductList />
+            <ProductList productList={products} />
         </div>
     )
 }
 
 Home.getLayout = function getLayout(page: ReactElement) {
     return <Layout>{page}</Layout>
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { data: products } = await axios.get('http://localhost:3000/api/product')
+    return {
+        props: {
+            products,
+        },
+    }
 }
