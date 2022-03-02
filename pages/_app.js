4628b94d7b8762dbd895b3f2b2a0700d5efdcd2e
papa-react <makiya112545@gmail.com>
2022-03-02 12:14:51 +0300

    wrap app component with redux provier

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+// style
+import '../styles/globals.css'
+import Layout from '../components/layout'
+import { store } from '../redux/store'
+import { Provider } from 'react-redux'
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <Provider store={store}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </Provider>
+    )
+}
+
+export default MyApp
