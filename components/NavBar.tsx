969d83bf4a228cc9179e8d8613d4e15ff645e7fa
papa-react <makiya112545@gmail.com>
2022-03-02 12:14:16 +0300

    select quantity

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { RootStateOrAny, useSelector } from 'react-redux'
 
 const NavBar = () => {
+    const quantity = useSelector((state: RootStateOrAny) => state.cart.quantity)
     return (
         <div className="sticky top-0 z-50 flex h-[70px] items-center justify-between bg-red-600 px-4 shadow-md md:px-5 xl:px-10">
             {/* left */}
@@ -58,7 +60,7 @@ const NavBar = () => {
                         alt="cart"
                     />
                     <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-white text-xs font-semibold text-red-600">
-                        3
+                        {quantity}
                     </span>
                 </div>
             </div>
