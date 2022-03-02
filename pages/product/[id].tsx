61c10aaa9064fd6a4b3fb23bf0bdbdf288242a1f
papa-react <makiya112545@gmail.com>
2022-03-02 12:17:02 +0300

    calculate price and extra price and handel add to cart

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -2,7 +2,9 @@ import axios from 'axios'
 import { GetServerSideProps } from 'next'
 import Image from 'next/image'
 import React, { ChangeEvent, ReactElement, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import Layout from '../../components/layout'
+import { addProduct } from '../../redux/cartSlice'
 import { IProduct } from '../../type'
 
 interface Props {
@@ -12,6 +14,10 @@ interface Props {
 export default function Product({ pizza }: Props) {
     const [size, setSize] = useState<number>(0)
     const [price, setPrice] = useState<number>(pizza.prices[size])
+    const [extra, setExtra] = useState<any>([])
+    const [quantity, setQuantity] = useState<number>(1)
+
+    const dispatch = useDispatch()
 
     const handelSize = (index: number) => {
         const deference = pizza.prices[index] - pizza.prices[size]
@@ -19,21 +25,20 @@ export default function Product({ pizza }: Props) {
         setPrice(price + deference)
     }
 
-    const handelChange = (
-        e: ChangeEvent<HTMLInputElement>,
-        extraPrice: number
-    ) => {
+    const handelChange = (e: ChangeEvent<HTMLInputElement>, option: any) => {
         const checked = e.target.checked
         if (checked) {
-            setPrice(price + extraPrice)
+            setPrice(price + option.price)
+            setExtra((prev: any) => [...prev, option])
         } else {
-            setPrice(price - extraPrice)
+            setPrice(price - option.price)
+            setExtra(extra.filter((item: any) => item._id !== option._id))
         }
     }
 
     return (
         <div className=" grid h-full grid-cols-1 pt-5 md:h-[95vh] md:grid-cols-2 md:py-10  xl:h-[calc(100vh_-_70px)]">
-            <div className="relative h-[250px] w-full md:h-[350px] xl:h-[500px] ">
+            <div className="relative h-[200px]  w-full md:h-[350px] xl:h-[500px] ">
                 <Image
                     layout="fill"
                     objectFit="contain"
@@ -41,8 +46,8 @@ export default function Product({ pizza }: Props) {
                     alt="logo"
                 />
             </div>
-            <div className=" flex flex-col space-y-3 px-2 md:mt-0 md:space-y-8 md:px-0 ">
-                <h1 className="text-3xl font-bold text-gray-800 md:text-5xl">
+            <div className=" flex flex-col space-y-3 px-2 pt-3 md:mt-0 md:space-y-8 md:px-0 ">
+                <h1 className="text-3xl font-bold capitalize text-gray-800 md:text-5xl">
                     {pizza.title}
                 </h1>
                 <span className="text-2xl font-semibold text-gray-600">
@@ -51,7 +56,7 @@ export default function Product({ pizza }: Props) {
                         birr
                     </span>
                 </span>
-                <p className=" text-base text-gray-600 md:pr-6  xl:pr-10">
+                <p className=" text-base font-semibold text-gray-500 md:pr-6  xl:pr-10">
                     {pizza.description}
                 </p>
                 <h3 className=" text-3xl font-bold text-gray-900 ">
@@ -109,7 +114,7 @@ export default function Product({ pizza }: Props) {
                     {pizza.extraOption.map((option, i: number) => (
                         <div className="flex items-center" key={i}>
                             <input
-                                onChange={(e) => handelChange(e, option.price)}
+                                onChange={(e) => handelChange(e, option)}
                                 className="h-5 w-5"
                                 type="checkbox"
                                 id={option.text}
@@ -129,18 +134,24 @@ export default function Product({ pizza }: Props) {
                     <input
                         type="number"
                         className="h-9 w-16 rounded-sm border border-gray-500 pl-[2px] text-xl "
-                        defaultValue={1}
+                        value={quantity}
+                        min={1}
+                        onChange={(e) => setQuantity(Number(e.target.value))}
                     />
-                    <button className="transform-gpu cursor-pointer bg-red-500 px-3 text-base font-semibold text-gray-50 shadow-lg outline-none transition duration-200 ease-in-out  hover:scale-105 active:scale-95">
+                    <button
+                        onClick={() =>
+                            dispatch(
+                                addProduct({ ...pizza, extra, quantity, price })
+                            )
+                        }
+                        className="transform-gpu cursor-pointer bg-red-500 px-3 text-base font-semibold text-gray-50 shadow-lg outline-none transition duration-200 ease-in-out  hover:scale-105 active:scale-95"
+                    >
                         Add To Cart
                     </button>
                 </div>
             </div>
         </div>
     )
-}
-Product.getLayout = function getLayout(page: ReactElement) {
-    return <Layout>{page}</Layout>
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
