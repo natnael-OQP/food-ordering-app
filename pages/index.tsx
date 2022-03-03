359a178fddbe9a5b57cb15f7e01581e42254d55a
papa-react <makiya112545@gmail.com>
2022-03-03 12:32:39 +0300

    addProduct button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import Featured from '../components/Featured'
 import Layout from '../components/layout'
 import ProductList from '../components/ProductList'
 import axios from 'axios'
 import { IProduct } from '../type'
+import AddButton from '../components/AddButton'
+import AddProduct from '../components/AddProduct'
+
 interface Props {
-    products: IProduct[],
+    products: IProduct[]
+    admin: boolean
 }
-export default function Home({ products }: Props) {
+export default function Home({ products, admin }: Props) {
+    const [close, setClose] = useState<boolean>(false)
+
     return (
         <div>
             <Head>
@@ -17,7 +23,9 @@ export default function Home({ products }: Props) {
                 <meta name="description" content="best Pizza Shop in Addise" />
             </Head>
             <Featured />
+            {admin && <AddButton setClose={setClose} />}
             <ProductList productList={products} />
+            {close && <AddProduct setClose={setClose} />}
         </div>
     )
 }
@@ -27,10 +35,18 @@ Home.getLayout = function getLayout(page: ReactElement) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { data: products } = await axios.get('http://localhost:3000/api/product')
+    const cookie = context.req?.cookies
+    let admin = false
+    if (cookie.token === process.env.TOKEN) {
+        admin = true
+    }
+    const { data: products } = await axios.get(
+        'http://localhost:3000/api/product'
+    )
     return {
         props: {
             products,
+            admin,
         },
     }
 }
