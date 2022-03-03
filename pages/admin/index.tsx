54e12dc2060339a392c3c45d7abca734191f008e
papa-react <makiya112545@gmail.com>
2022-03-03 09:35:01 +0300

    remove orders

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -3,16 +3,13 @@ import { GetServerSideProps } from 'next'
 import Image from 'next/image'
 import React, { FC, useState } from 'react'
 import { IProduct, IOrder } from '../../type'
-import product from '../api/product'
 
 interface Props {
     products: IProduct[]
-    orders: IOrder[]
 }
 
-const Admin: FC<Props> = ({ products, orders }) => {
+const Admin: FC<Props> = ({ products }) => {
     const [pizza, setPizza] = useState<IProduct[]>(products)
-    const [order, setOrder] = useState<IProduct[]>(products)
 
     // optimistic ui
     const handelDelete = async (id: string) => {
@@ -21,7 +18,7 @@ const Admin: FC<Props> = ({ products, orders }) => {
     }
 
     return (
-        <div className="width full grid grid-cols-1 gap-y-10 pt-5 pb-40 xl:min-h-[calc(100vh_-_110px)] xl:grid-cols-2 xl:pb-20">
+        <div className="width full grid grid-cols-1 gap-y-10 pt-5 pb-40 md:min-h-[400px] xl:min-h-[calc(100vh_-_110px)] xl:grid-cols-2 xl:pb-20">
             {/* product */}
             <div className="pl-2 xl:pl-6">
                 <h1 className="py-6 text-3xl font-bold text-gray-700 md:text-4xl">
@@ -89,56 +86,6 @@ const Admin: FC<Props> = ({ products, orders }) => {
                     ))}
                 </table>
             </div>
-            {/* order */}
-            <div className="pl-2 xl:pl-6">
-                <h1 className="py-6 text-3xl font-bold text-gray-700 md:text-5xl">
-                    Orders
-                </h1>
-                <table className="w-full  text-left">
-                    <tbody>
-                        <tr>
-                            <th className="pl-3">ID</th>
-                            <th className="pl-3">Customer</th>
-                            <th className="pl-3">Total</th>
-                            <th className="pl-3">Status</th>
-                            <th className="pl-3">Action</th>
-                        </tr>
-                    </tbody>
-                    {/* table data */}
-                    <tbody>
-                        <tr>
-                            <td className="pr-1">
-                                <span className="text-sm font-medium text-gray-500">
-                                    {'09121928'.slice(0, 6) + '...'}
-                                </span>
-                            </td>
-                            <td className="pr-1">
-                                <span className="text-sm font-medium text-gray-500">
-                                    Natnael
-                                </span>
-                            </td>
-                            <td className="pr-1">
-                                <span className="text-sm font-medium text-gray-800">
-                                    19
-                                </span>{' '}
-                                <span className="text-xs font-normal text-gray-400 ">
-                                    birr
-                                </span>
-                            </td>
-                            <td className="pr-1">
-                                <span className="text-sm font-medium text-gray-500">
-                                    Preparing
-                                </span>
-                            </td>
-                            <td className="pr-1">
-                                <button className="rounded-sm border-none bg-amber-500 px-3 py-[2px] text-base font-semibold text-white shadow-sm">
-                                    Next Stage
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
         </div>
     )
 }
@@ -147,12 +94,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const { data: products } = await axios.get(
         'http://localhost:3000/api/product'
     )
-    // const { data: orders } = await axios.get('http://localhost:3000/api/orders')
 
     return {
         props: {
             products,
-            // orders,
         },
     }
 }
