710af48fb510c41c65e1e8b660256ae230c4a5c3
papa-react <makiya112545@gmail.com>
2022-03-03 09:35:24 +0300

    create login api

---
+++ pages/api/login.js
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from '../../lib/dbConnect'
+import cookie from 'cookie'
+
+export default async function handler(req, res) {
+    const { method } = req
+    dbConnect()
+
+    if (method === 'POST') {
+        const { username, password } = req.body
+        if (
+            username === process.env.USERNAME &&
+            password === process.env.PASSWORD
+        ) {
+            res.setHeader(
+                'Set-Cookie',
+                cookie.serialize('token', process.env.TOKEN, {
+                    MaxAge: 60 * 60,
+                    sameSite: 'strict',
+                    path: '/',
+                })
+            )
+            res.status(200).json('Successful')
+        }
+    } else {
+        res.status(400).json('error')
+    }
+}
